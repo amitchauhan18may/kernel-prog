f117877af7b4f2d56dc050b765bce1268856bb9b
Amit Chauhan <amitchauhan18may@gmail.com>
2018-07-10 01:27:00 +0530

    Character Driver Compilation with successfull run

--- K-Coding/character_driver/chardev.c
+++ K-Coding/character_driver/chardev.c
@@ -1,1 +1,21 @@
 /*Writing Charcter Device Driver For User-Space Communication*/
+#include <linux/module.h>
+#include <linux/init.h>
+
+static int __init my_init (void)
+{
+	printk(KERN_INFO "Hello: module loaded at 0x%p\n", my_init);
+	return 0;
+}
+
+static void __exit my_exit (void)
+{
+	printk(KERN_INFO "Bye: module unloaded at 0x%p\n", my_exit);
+}
+
+module_init (my_init);
+module_exit (my_exit);
+
+MODULE_AUTHOR ("Amit Chauhan");
+MODULE_DESCRIPTION ("Test character driver module");
+MODULE_LICENSE ("GPL");
