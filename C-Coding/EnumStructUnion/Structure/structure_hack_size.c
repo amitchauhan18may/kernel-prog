67a1e0760865952516892758cfeb2870445cc43a
Amit Chauhan <amitchauhan18may@gmail.com>
2018-07-22 19:22:48 +0530

    Structure: Sizeof arr[0] in structure considered as 0-bytes

---
+++ C-Coding/EnumStructUnion/Structure/structure_hack_size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+struct struct_a {
+    int a;
+    int b;
+    char arr[0];
+};
+
+/* In gcc, when we create an array of zero length,
+ * it is considered as array of incomplete type that’s why
+ * gcc reports its size as “0” bytes.*/
+int main () {
+    struct struct_a s_a;
+    printf("struct_a {\n"
+                "\tint a;\n"
+                "\tint b;\n"
+                "\tchar arr[0];\n"
+           "}\n""Size of struct_a = %lu\n", sizeof(s_a));
+    return 0;
+}
