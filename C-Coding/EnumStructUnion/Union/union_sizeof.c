7728b4aaa510d0fec13ede0eabe3f6b259d80963
Amit Chauhan <amitchauhan18may@gmail.com>
2018-07-22 18:45:05 +0530

    Union: Sizeof demonstration on union

---
+++ C-Coding/EnumStructUnion/Union/union_sizeof.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+union union1 {
+    int a;/*Maximum size of union member = 4*/
+    char b;
+    float c;
+};
+
+union union2 {
+    int a[50];/*Maximum size of union member 4 x 50 = 200*/
+    double d;
+};
+
+
+int main () {
+    printf("Sizeof union1: %ld\n", sizeof(union union1));
+    printf("Sizeof union2: %ld\n", sizeof(union union2));
+    return 0;
+}
