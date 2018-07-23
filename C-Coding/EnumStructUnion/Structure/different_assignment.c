6ffe20b1fb6893ad31f9294c35780cb1f5b1ccec
Amit Chauhan <Amit.Kumar8@harman.com>
2018-07-23 12:37:13 +0530

    Structure: Assignment and comparision between two objects of same structure

--- C-Coding/EnumStructUnion/Structure/different_assignment.c
+++ C-Coding/EnumStructUnion/Structure/different_assignment.c
@@ -7,6 +7,7 @@ struct details {
 };
 
 int main (void) {
+    struct details assign_detail;
 /*Designated initialization of structure*/
 #if 1
     struct details my_detail = {
@@ -15,11 +16,25 @@ int main (void) {
         .salary_pm = 150000.5
     };
 #else
-/*Initializer list initialization of structure*/  
+/*Initializer list initialization of structure*/
     struct details my_detail = {"Amit Chauhan", 29, 150000.5};
 #endif
     printf("Details:\nName: %s\nAge: %d\nSalary/Month: %f\n",
             my_detail.name, my_detail.age, my_detail.salary_pm);
 
+
+/*Assignment and comparision between two objects of same structure*/
+    assign_detail = my_detail;
+    printf("\nAssigned Details:\nName: %s\nAge: %d\nSalary/Month: %f\n",
+            assign_detail.name, assign_detail.age, assign_detail.salary_pm);
+
+/*Comparision
+ * error: invalid operands to binary == (have ‘struct details’ and ‘struct details’)
+    if (assign_detail == my_detail) {
+        printf("\nassign_detail == my_detail\n");
+    } else {
+        printf("\nassign_detail != my_detail\n");
+    }
+ */
     return 0;
 }
