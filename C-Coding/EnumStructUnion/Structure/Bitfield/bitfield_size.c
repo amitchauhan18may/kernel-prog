1bd04776ad483f1f1e6f13aa74002989ba874760
Amit Chauhan <amitchauhan18may@gmail.com>
2018-07-25 09:19:05 +0530

    Struct Bitfield: size fundamental

--- C-Coding/EnumStructUnion/Structure/Bitfield/bitfield_size.c
+++ C-Coding/EnumStructUnion/Structure/Bitfield/bitfield_size.c
@@ -10,7 +10,23 @@ struct bitfield1 {
     int pad2:25;   /* pad to 32 bits */
 };
 
+struct bitfield2 {
+    int bigfield1:31;     /* 32-bit word 1 begins */
+    int bigfield2:31;     /* 32-bit word 2 begins */
+    int littlefield1:1;
+    int littlefield2:1;   /* 32-bit word 3 begins */
+};
+
+struct bitfield3 {
+    int bigfield1:31;     /* 32-bit word 1 begins */
+    int littlefield1:1;
+    int bigfield2:31;     /* 32-bit word 2 begins */
+    int littlefield2:1;
+};
+
 int main () {
     printf("Size of bitfield1: %lu-bytes\n", sizeof(struct bitfield1));
+    printf("Size of bitfield2: %lu-bytes\n", sizeof(struct bitfield2));
+    printf("Size of bitfield3: %lu-bytes\n", sizeof(struct bitfield3));
     return 0;
 }
