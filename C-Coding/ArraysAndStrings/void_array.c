1e8ef8d7322219f94df6d0dec8d4a540d17d855a
Amit Chauhan <Amit.Kumar8@harman.com>
2018-08-20 09:33:13 +0530

    ArrayAndStrings: Array of void is not allowed in c

---
+++ C-Coding/ArraysAndStrings/void_array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () {
+/*Below instruction will throw an error
+ *error: declaration of ‘arr’ as array of voids
+ *This singnifies, we can not declear an array of void
+ */
+    void arr[10];
+    return 0;
+}
