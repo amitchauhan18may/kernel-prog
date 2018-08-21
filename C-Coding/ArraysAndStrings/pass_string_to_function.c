4184e57cd093d5bc202466ea480deb66435209c1
Amit Chauhan <amitchauhan18may@gmail.com>
2018-08-21 07:08:16 +0530

    Array always passed as pointer to funtion

--- C-Coding/ArraysAndStrings/pass_string_to_function.c
+++ C-Coding/ArraysAndStrings/pass_string_to_function.c
@@ -3,14 +3,17 @@
 #if 0
 int test_strinf_func (char *string) {
 #else
-int test_strinf_func (char string []) {
+/* Array parameters are always passed as pointers,
+ * even when we use square brackets*/
+int test_string_func (char string[]) {
 #endif
+    printf("sizeof(string) = %zu\n", sizeof(string));
     printf("String: %s\n", string);
 }
 
 int main () {
     char string[] = "Amit Chauhan";
-    test_strinf_func(string);
+    test_string_func(string);
 
     return 0;
 }
