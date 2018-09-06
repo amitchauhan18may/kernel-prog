120e8800ddd0ffeaf0a60085977da790cf8ace01
Amit Chauhan <amitchauhan18may@gmail.com>
2018-09-06 20:09:33 +0530

    Inotify test script

---
+++ inotify/test_script/test.sh
@@ -0,0 +1,9 @@
+while true
+do
+    echo 1 > /home/amit/test_home.text
+    sleep 1
+    echo 1 > /home/amit/Documents/test_doc.text
+    sleep 1
+    echo 1 > /home/amit/Downloads/test_download.text
+    sleep 1
+done
