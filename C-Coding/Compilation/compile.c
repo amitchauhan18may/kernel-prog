1fdbfa0d8c2e5625a927681caebef00e64d1bdbe
Amit Chauhan <amitchauhan18may@gmail.com>
2018-10-04 20:49:31 +0530

    C code compilation in detail

---
+++ C-Coding/Compilation/compile.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* code compilation should be done as
+ * gcc –Wall –save-temps compile.c –o compile 
+ */
+
+int main (void) {
+    printf("Hello World\n");
+    return 0;
+}
