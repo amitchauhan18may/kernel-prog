7c755d42abcedb7dbd93065dc4ec489f7fccddd2
Amit Chauhan <amitchauhan18may@gmail.com>
2018-10-05 05:31:50 +0530

    MultiDimensional allocation using single_pointer - 2

--- C-Coding/ArraysAndStrings/MultiDimensional/Allocation/single_pointer.c
+++ C-Coding/ArraysAndStrings/MultiDimensional/Allocation/single_pointer.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#define NEW 0
 
 int main () {
     int row = 3;
@@ -14,22 +15,31 @@ int main () {
         printf("%s\n", strerror(errno));
         return errno;
     }
-
+/* Assigning values */
+#if 0
     for (i = 0; i < row; i++) {
         for (j = 0; j < column; j++) {
+#else
+    for (i = 0; i < row*column; i++) {
+#endif
             printf("Count:");
             printf("%d, ", count);
             printf("\n");
+#if 0
             *(arr + i*column + j) = ++count;
+#endif
+            *(arr + i) = ++count;
         }
-    }
-
+   // }
+/* Printing values */
     for (i = 0; i < row; i++) {
         for (j = 0; j < column; j++) {
-            printf("%d, ", *(arr + i*column + j)); 
+            printf("%d, ", *(arr + i*column + j));
             printf("\n");
         }
     }
+
+    free(arr);
 
     return 0;
 }
