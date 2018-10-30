3f365532d6ee8ce3897364a59ab5f79dc123810c
Amit Chauhan <Amit.Kumar8@harman.com>
2018-10-30 10:12:09 +0530

    Array: recursive reverse

--- C-Coding/ArraysAndStrings/reverse_string.c
+++ C-Coding/ArraysAndStrings/reverse_string.c
@@ -14,8 +14,34 @@ char* reverse_string (char* reverse) {
     return reverse;
 }
 
+<<<<<<< HEAD
+void recursive_reverse (char *reverse, int start, int end) {
+    char temp;
+
+    if (start > end)
+        return;
+    temp = *(reverse + start);
+    *(reverse + start) = *(reverse + end);
+    *(reverse + end) = temp;
+
+    recursive_reverse(reverse, ++start, --end);
+}
+
+int main () {
+    char string[] = "abcd";
+    int len = strlen(string);
+#if 0
+    reverse_string(string);
+#else
+    recursive_reverse(string, 0, len - 1);
+#endif
+
+    printf("After reverse: %s\n", string);
+
+=======
 int main () {
     char string[] = "abcd";
     reverse_string(string);
+>>>>>>> 86a8788b3c3261c38dc2cdbc9515be4a30b75486
     return 0;
 }
