cf3d7afcc035ec97b2539dbbf6f7cc10211502b9
Amit Chauhan <amitchauhan18may@gmail.com>
2018-11-18 19:33:52 +0530

    Pointer: NULL fundamental

--- C-Coding/Pointers/size_of_NUL.c
+++ C-Coding/Pointers/size_of_NUL.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* NULL - 0x00000000 or 0x00000000'00000000 (32 vs 64 bit)
- * NUL  - 0x00 or 0x0000 (ascii vs 2byte unicode)
- * '0'  - 0x20 (integer character constant)
+/* NULL or pointer - 0x00000000 or 0x00000000'00000000 (32 vs 64 bit)
+ * NUL or '\0'  - 0x00 or 0x0000 (ascii vs 2byte unicode)
+ * '0' or space - 0x20 (integer character constant space)
  *  0   - 0x30 (48 in Decimal)
  */
 
@@ -11,6 +11,9 @@
 int main (void) {
 
     printf("Size-of NUL: %ld\n", sizeof('\0'));
+
+    printf("space=%c\n", 0);
+    printf("decimal of 0 = %d: %c\n", 0x30, 0x30);
     return 0;
 
 }
