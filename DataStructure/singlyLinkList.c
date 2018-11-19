b7272feb0c99ce9f1e4021be8ce9e0f4a3b713a7
Amit Chauhan <Amit.Kumar8@harman.com>
2018-11-19 09:49:33 +0530

    Singly LinkList: Push on end | nth location

--- DataStructure/singlyLinkList.c
+++ DataStructure/singlyLinkList.c
@@ -10,7 +10,7 @@ void print_list (struct node *head) {
 
     struct node *temp = head;
 
-    while (temp != NULL) {
+    while (NULL != temp) {
         printf("%d -> ", temp->data);
         temp = temp->next;
     }
@@ -26,8 +26,59 @@ void push_on_front (struct node **head, int data) {
 
     new->data = data;
     new->next = *head;
-
     *head = new;
+
+    return;
+}
+
+void push_on_end (struct node **head, int data) {
+
+    struct node *temp = *head;
+    struct node *new  = (struct node *) malloc(sizeof(struct node));
+
+    new->data = data;
+    new->next = NULL;
+
+    if (NULL == *head) {
+        *head = new;
+        return;
+    }
+
+    while (NULL != temp->next) {
+        temp = temp->next;
+    }
+
+    temp->next = new;
+
+    return;
+}
+
+void push_on_nth (struct node **head, int data, int pos) {
+
+    if (NULL == *head) {
+        printf("Empty LinkLIst");
+        return;
+    }
+
+    struct node *temp_head, *temp;
+    temp_head = *head;
+    int i = 1;
+    struct node *new  = (struct node *) malloc(sizeof(struct node));
+
+    new->data = data;
+
+    while (i < pos-1) {
+        if (NULL == temp_head->next){
+            printf("No specified location found!\n");
+            return;
+        }
+        temp_head = temp_head->next;
+        i++;
+    }
+
+    temp = temp_head->next;
+    temp_head->next = new;
+    new->next = temp;
 
     return;
 }
@@ -53,7 +104,12 @@ int main (void) {
     print_list(head);
 
     push_on_front(&head, 15);
+    print_list(head);
 
+    push_on_end(&head, 25);
+    print_list(head);
+
+    push_on_nth (&head, 112, 9);
     print_list(head);
 
     return 0;
