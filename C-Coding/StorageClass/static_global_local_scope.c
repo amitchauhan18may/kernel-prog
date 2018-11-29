47978062770b1f92f04906d94a96313f628b5f8f
Amit Chauhan <amitchauhan18may@gmail.com>
2018-11-29 17:48:56 +0530

    Storage Class: Static local and global scope2

--- C-Coding/StorageClass/static_global_local_scope.c
+++ C-Coding/StorageClass/static_global_local_scope.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-static int g_val = 66;
+/* On commenting below statement compiler will throwh below error
+ * static_global_local_scope.c:21:26: error: ‘g_val’ undeclared (first use in this function)
+     printf("func: %d\n", g_val);
+ *
+ */
+//static int g_val = 66;
 
 void func(void);
 
