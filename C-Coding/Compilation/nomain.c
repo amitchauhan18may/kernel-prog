59ffef5086381267035589a8409cbf1fdc8d147c
Amit Chauhan <amitchauhan18may@gmail.com>
2019-07-28 02:25:04 +0530

    Code without main

---
+++ C-Coding/Compilation/nomain.c
@@ -0,0 +1,14 @@
+/*gcc -nostartfiles -o nomain nomain.c*/
+#include<stdio.h> 
+#include<stdlib.h> 
+void _start() 
+{ 
+    int x = my_fun(); //calling custom main function 
+    exit(x); 
+} 
+  
+int my_fun() // our custom main function 
+{ 
+    printf("Hello world!\n"); 
+    return 0; 
+} 
