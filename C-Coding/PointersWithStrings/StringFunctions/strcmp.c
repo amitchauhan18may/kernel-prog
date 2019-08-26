327dbf336e0b0c370371b83e43622aa2c0e220ee
Amit Chauhan <amitchauhan18may@gmail.com>
2019-08-26 22:15:46 +0530

    StringFunctions: string compare with any case

--- C-Coding/PointersWithStrings/StringFunctions/strcmp.c
+++ C-Coding/PointersWithStrings/StringFunctions/strcmp.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include <ctype.h>
+
 
 int xstrcmp (char *dest, char *source) {
 
     while (*source != '\0') {
         if (*dest != *source)
+            return -1;
+        source++;
+        dest++;
+    }
+
+    return 0;
+}
+
+
+int xstrcmpi (char *dest, char *source) {
+
+    while (*source != '\0') {
+        if (toupper(*dest) != toupper(*source))
             return -1;
         source++;
         dest++;
@@ -19,9 +34,11 @@ int main (void) {
     char *str2 = "AmitJi";
 
     int result = xstrcmp(str1, str2);
+    int resulti = xstrcmpi(str1, str2);
 
     printf("Str1: %s\nStr2: %s\n", str1, str2); 
     printf("After xstrcmp: %d\n", result);
+    printf("After xstrcmpi: %d\n", resulti);
 
     return 0;
 
