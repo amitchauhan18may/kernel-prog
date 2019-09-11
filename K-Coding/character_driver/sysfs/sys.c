0bee887a4aaaf86ee343d09801f0ba4a3d7fcd65
Amit Chauhan <Amit.Kumar8@harman.com>
2019-09-11 17:18:29 +0530

    Driver: sysfs

--- K-Coding/character_driver/sysfs/sys.c
+++ K-Coding/character_driver/sysfs/sys.c
@@ -1,10 +1,31 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
+#include <linux/sysfs.h>
+#include <linux/kobject.h>
 
 static dev_t dev;
 static struct cdev my_cdev;
 static struct class *my_cls;
+
+volatile int sys_val = 0;
+struct kobject *sys_koj;
+
+static ssize_t sysfs_show(struct kobject *kobj, struct kobj_attribute *kobj_attr, char *buf)
+{
+	pr_info("sysfs_show value: %d\n", sys_val);
+
+	return 0;
+}
+
+static ssize_t sysfs_store(struct kobject *kobj, struct kobj_attribute *kobj_attr, const char *buf, size_t count)
+{
+	pr_info("sysfs_show value: %p\n", &sys_val);
+
+	return 0;
+}
+
+struct kobj_attribute attribute = __ATTR(sys_val, 0660, sysfs_show, sysfs_store);
 
 static int my_open (struct inode *inode, struct file *file)
 {
@@ -67,13 +88,24 @@ static int __init sys_init (void)
                 goto r_cls;
         }
 
+	sys_koj = kobject_create_and_add("amit-sysfs", kernel_kobj);
+
+	if (sysfs_create_file(sys_koj, &attribute.attr)) {
+		pr_err("Failed to create sysfs file\n");
+		goto r_sysfs;
+	}
+
         pr_info("Sysfs module loaded succesfully\n");
 
         return 0;
 
+r_sysfs:
+	kobject_put(sys_koj);
+	sysfs_remove_file(sys_koj, &attribute.attr);
 r_cls:
         class_destroy(my_cls);
 r_dev:
+	cdev_del(&my_cdev);
         unregister_chrdev_region(dev, 1);
 
         return -1;
@@ -81,8 +113,11 @@ r_dev:
 
 static void __exit sys_exit (void)
 {
+	kobject_put(sys_koj);
+	sysfs_remove_file(sys_koj, &attribute.attr);
         device_destroy(my_cls, dev);
         class_destroy(my_cls);
+	cdev_del(&my_cdev);
         unregister_chrdev_region(dev, 1);
         pr_info("Sysfs module unloaded succesfully\n");
 
