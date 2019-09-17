f1bff68d789242a94986d6e9f1afb378fb6defe7
Amit Chauhan <amitchauhan18may@gmail.com>
2019-09-17 20:13:59 +0530

    Driver: Interrupt

--- K-Coding/interrupt/interrupt.c
+++ K-Coding/interrupt/interrupt.c
@@ -1,109 +1,120 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
+#include <linux/interrupt.h>
+#include <asm/io.h>
+
 #define IRQ_NO 11
 
 static dev_t dev;
 static struct class *my_class;
 static struct cdev my_cdev;
 
+static irqreturn_t irq_handler (int irq, void *dev_id)
+{
+        pr_info("AMIT:Shared IRQ: Interrupt occured\n");
+        return IRQ_HANDLED;
+}
+
 static int my_open (struct inode *inode, struct file *file)
 {
-	pr_info("Opening device\n");
-	return 0;
+        pr_info("Opening device\n");
+        return 0;
 }
 
 static int my_release (struct inode *inode, struct file *file)
 {
-	pr_info("Closing device\n");
-	return 0;
+        pr_info("Closing device\n");
+        return 0;
 }
 
 ssize_t my_read (struct file *file, char __user *buf, size_t len, loff_t *offset)
 {
-	pr_info("Reading device\n");
-
-	return len;
+        pr_info("Reading device\n");
+        asm("int $0x3B");
+        return 0;
 }
 
 ssize_t my_write (struct file *file, const char __user *buf, size_t len, loff_t *offset)
 {
-	pr_info("Writing device\n");
+        pr_info("Writing device\n");
 
-	return len;
+        return len;
 }
 
 static struct file_operations my_fops =
 {
-	owner: THIS_MODULE,
-	open: my_open,
-	read: my_read,
-	write: my_write,
-	release: my_release
+        owner: THIS_MODULE,
+       open: my_open,
+       read: my_read,
+       write: my_write,
+       release: my_release
 };
 
 static int __init my_interrupt_init (void)
 {
-	if (alloc_chrdev_region(&dev, 0, 1, "amit-dev") < 0) {
-		pr_err("failed to allocate major number\n");
-		return -1;
-	}
+        if (alloc_chrdev_region(&dev, 0, 1, "amit-dev") < 0) {
+                pr_err("failed to allocate major number\n");
+                return -1;
+        }
 
-	pr_info("Major No: %d Minor No: %d\n", MAJOR(dev), MINOR(dev));
+        pr_info("Major No: %d Minor No: %d\n", MAJOR(dev), MINOR(dev));
 
-	cdev_init(&my_cdev, &my_fops);
-	my_cdev.owner = THIS_MODULE;
+        cdev_init(&my_cdev, &my_fops);
+//        my_cdev.owner = THIS_MODULE;
 
-	if (cdev_add(&my_cdev, dev, 1) < 0)
-       	{
-		pr_err("Failed to add device\n");
-		goto r_device;
-	}
+        if (cdev_add(&my_cdev, dev, 1) < 0)
+        {
+                pr_err("Failed to add device\n");
+                goto r_device;
+        }
 
-	if ((my_class = class_create(THIS_MODULE, "amit-class")) == NULL)
-	{
-		pr_err("Failed to create class\n");
-		goto r_class;
-	}
+        if ((my_class = class_create(THIS_MODULE, "amit-class")) == NULL)
+        {
+                pr_err("Failed to create class\n");
+                goto r_class;
+        }
 
-	if (device_create(my_class, NULL, dev, NULL, "amit-device") == NULL)
-	{
-		pr_err("Failed to create device\n");
-		goto r_class;
-	}
+        if (device_create(my_class, NULL, dev, NULL, "amit-device") == NULL)
+        {
+                pr_err("Failed to create device\n");
+                goto r_class;
+        }
 
-	if ()
-	{
-		pr_err("Failed register Interrupt\n");
-		goto r_intr;
-	}
+        if (request_irq(IRQ_NO, irq_handler, IRQF_SHARED, "amit-device",
+                                (void *)(irq_handler)))
+        {
+                pr_err("Failed register Interrupt\n");
+                goto r_intr;
+        }
 
-	pr_info("Interrrupt module loaded successfully\n");
+        pr_info("Interrrupt module loaded successfully\n");
 
-	return 0;
+        return 0;
 r_intr:
-
+        free_irq(IRQ_NO, (void *)(irq_handler));
 
 r_class:
-	class_destroy(my_class);
+        class_destroy(my_class);
 
 r_device:
-	cdev_del(&my_cdev);
-	unregister_chrdev_region(dev, 1);
+        cdev_del(&my_cdev);
+        unregister_chrdev_region(dev, 1);
 
-	return -1;
+        return -1;
 }
 
 
 static void __exit my_interrupt_exit (void)
 {
-	device_destroy(my_class, dev);
-	class_destroy(my_class);
-	cdev_del(&my_cdev);
-	unregister_chrdev_region(dev, 1);
+        free_irq(IRQ_NO, (void *)(irq_handler));
+        device_destroy(my_class, dev);
+        class_destroy(my_class);
+        cdev_del(&my_cdev);
+        unregister_chrdev_region(dev, 1);
 
-	pr_info("Interrrupt module un-loaded successfully\n");
-	return;
+        pr_info("Interrrupt module un-loaded successfully\n");
+        return;
 }
 
 module_init(my_interrupt_init);
