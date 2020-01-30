0ed742e3d7ddc265e933cc8ee65185112054a0fd
Amit Chauhan <Amit.Kumar8@harman.com>
2020-01-30 15:59:40 +0530

    Pthread Attribute

---
+++ Synchronization/PosixThread/thread_attr.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdbool.h>
+bool thread_finished = 0;
+
+void* start_function(void* value)
+{
+    printf("%s is now entering the thread function.\n", (char*)value);
+    sleep(4);
+    thread_finished =1;
+    printf("%s is now leaving the thread function.\n", (char*)value);
+    pthread_exit(value);
+}
+
+int main(void)
+{
+    int res,err;
+    pthread_attr_t attr;
+    pthread_t thread1;
+
+    res = pthread_attr_init(&attr);
+    if (res != 0) {
+        perror("Attribute init failed");
+        exit(EXIT_FAILURE);
+    }
+
+    res = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+    if (res != 0) {
+        perror("Setting detached state failed");
+        exit(EXIT_FAILURE);
+    }
+
+    res = pthread_create(&thread1, &attr, start_function, (void*)"Thread1");
+    if (res != 0) {
+        perror("Creation of thread failed");
+        exit(EXIT_FAILURE);
+    }
+    pthread_join(thread1, NULL);
+    while(!thread_finished) {
+        printf("Waiting for thread1 to finish.\n");
+        sleep(1);
+    }
+
+    printf("Child thread finished.\n");
+    pthread_attr_destroy(&attr);
+}
+
