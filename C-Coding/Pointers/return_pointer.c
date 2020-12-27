a5681ff0f2bfa9f43bb11a9c8975a9b4637ffa8d
Amit Chauhan <amitchauhan18may@gmail.com>
2020-12-27 19:05:54 +0530

    Segmentaion fault while returning pointer without declaring as Static

--- C-Coding/Pointers/return_pointer.c
+++ C-Coding/Pointers/return_pointer.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
+extern int j = 0;
 int *func (void) {
 /* If we won't declare as static,
  * it will give segmentation fault
  */
     static int i = 25;
-   
-    printf("Func() Address of i: %p\n", &i);
+    printf("Func() Address of i: %p : %d\n", &i, i);
     return (&i);
 }
 
@@ -15,7 +15,8 @@ int main () {
     int *i;
 
     i = func();
-
+    *i = 2;
+    func();
     printf("Main() Address of i: %p\n", i);
     printf("Value: %d\n", *i);
 
